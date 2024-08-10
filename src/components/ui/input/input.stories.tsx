896a95d9990c680a1ca06481ee8a241f68a613db
edwinhati <edwinhati@outlook.com>
2024-08-11 01:05:37 +0700

    docs: Add input component story

---
+++ src/components/ui/input/input.stories.tsx
@@ -0,0 +1,21 @@
+import { fn } from "@storybook/test";
+import { Input } from "@/components/ui/input";
+import type { Meta, StoryObj } from "@storybook/react";
+
+const meta = {
+  component: Input,
+  title: "UI/Input",
+  parameters: {
+    layout: "centered",
+  },
+  tags: ["autodocs"],
+  args: { onClick: fn() },
+} satisfies Meta<typeof Input>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
