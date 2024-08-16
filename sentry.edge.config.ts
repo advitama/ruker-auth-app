af315636e49c2969f5149acde4e72c986dc80fd8
edwinhati <edwinhati@outlook.com>
2024-08-17 01:25:34 +0700

    chore: Update Sentry configuration for different environments

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -6,6 +6,7 @@
 import * as Sentry from "@sentry/nextjs";
 
 Sentry.init({
+  environment: process.env.NODE_ENV,
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
   // Adjust this value in production, or use tracesSampler for greater control
