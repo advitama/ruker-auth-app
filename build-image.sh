f779e6e88a5d47cb1e90428959b392d7aff16e5f
edwinhati <edwinhati@outlook.com>
2024-10-13 19:58:40 +0700

    chore: fix build command

--- build-image.sh
+++ build-image.sh
@@ -2,7 +2,7 @@
 export $(grep -v '^#' .env | xargs)
 
 # Build the Docker image
-docker build --platform \
+docker build \
   --build-arg NEXT_PUBLIC_POSTHOG_KEY=$NEXT_PUBLIC_POSTHOG_KEY \
   --build-arg NEXT_PUBLIC_POSTHOG_HOST=$NEXT_PUBLIC_POSTHOG_HOST \
   --build-arg NEXT_PUBLIC_AUTH_API_URL=$NEXT_PUBLIC_AUTH_API_URL \
