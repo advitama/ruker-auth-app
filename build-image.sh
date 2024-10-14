3b7b362add30d410c4c7f35b2f73a644697e89e8
Edwin Hati <edwinhati@outlook.com>
2024-10-14 09:04:17 +0000

    chore: add BASE_DOMAIN to arg and env in docker

--- build-image.sh
+++ build-image.sh
@@ -3,6 +3,7 @@ export $(grep -v '^#' .env | xargs)
 TAG=${2:-latest}
 
 docker build \
+  --build-arg BASE_DOMAIN=$BASE_DOMAIN \
   --build-arg NEXT_PUBLIC_BASE_PATH=$NEXT_PUBLIC_BASE_PATH \
   --build-arg NEXT_PUBLIC_POSTHOG_KEY=$NEXT_PUBLIC_POSTHOG_KEY \
   --build-arg NEXT_PUBLIC_POSTHOG_HOST=$NEXT_PUBLIC_POSTHOG_HOST \
